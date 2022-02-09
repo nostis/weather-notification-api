1016b5efc1e96ddf6066b980dd8cbd5e1e9c5ea0
nostis <norbert_2000@o2.pl>
2022-02-09 18:43:07 +0100

    Add user profile entity along with username property and link this to user entity

---
+++ src/Entity/UserProfile.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserProfileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserProfileRepository::class)
+ */
+class UserProfile
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $username;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, inversedBy="userProfile", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private User $userRelation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getUserRelation(): ?User
+    {
+        return $this->userRelation;
+    }
+
+    public function setUserRelation(User $userRelation): self
+    {
+        $this->userRelation = $userRelation;
+
+        return $this;
+    }
+}
