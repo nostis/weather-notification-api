bac92c453ed9586d767e8be8d49ec08b5fc787a9
nostis <norbert_2000@o2.pl>
2022-02-11 18:15:43 +0100

    Transform input dto to target entity

--- src/DataTransformer/User/UserRegisterInputDataTransfomer.php
+++ src/DataTransformer/User/UserRegisterInputDataTransfomer.php
@@ -6,14 +6,17 @@ use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
 use ApiPlatform\Core\Validator\ValidatorInterface;
 use App\Dto\User\UserRegisterInput;
 use App\Entity\User;
+use App\Service\User\UserRegisterService;
 
 class UserRegisterInputDataTransfomer implements DataTransformerInterface
 {
     private ValidatorInterface $validator;
+    private UserRegisterService $userRegisterService;
 
-    public function __construct(ValidatorInterface $validator)
+    public function __construct(ValidatorInterface $validator, UserRegisterService $userRegisterService)
     {
         $this->validator = $validator;
+        $this->userRegisterService = $userRegisterService;
     }
 
     /**
@@ -22,6 +25,8 @@ class UserRegisterInputDataTransfomer implements DataTransformerInterface
     public function transform($object, string $to, array $context = [])
     {
         $this->validator->validate($object);
+
+        return $this->userRegisterService->createUser($object->email, $object->plainPassword, $object->name);
     }
 
     public function supportsTransformation($data, string $to, array $context = []): bool
