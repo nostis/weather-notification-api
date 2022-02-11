7858bfb8881a01d5c908e390a70b41f500bec1b1
nostis <norbert_2000@o2.pl>
2022-02-11 17:17:53 +0100

    Change property name

--- src/Entity/UserProfile.php
+++ src/Entity/UserProfile.php
@@ -20,7 +20,7 @@ class UserProfile
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private string $username;
+    private string $name;
 
     /**
      * @ORM\OneToOne(targetEntity=User::class, inversedBy="userProfile", cascade={"persist", "remove"})
@@ -33,14 +33,14 @@ class UserProfile
         return $this->id;
     }
 
-    public function getUsername(): ?string
+    public function getName(): ?string
     {
-        return $this->username;
+        return $this->name;
     }
 
-    public function setUsername(?string $username): self
+    public function setUsername(?string $name): self
     {
-        $this->username = $username;
+        $this->name = $name;
 
         return $this;
     }
