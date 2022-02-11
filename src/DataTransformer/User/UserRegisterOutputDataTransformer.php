e7ac4a53644e1d29738107f30b0fb76c5448b4a1
nostis <norbert_2000@o2.pl>
2022-02-11 18:25:55 +0100

    Add transformer for user to return as user dto and add service definition

---
+++ src/DataTransformer/User/UserRegisterOutputDataTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataTransformer\User;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\User\UserRegisterOutput;
+use App\Entity\User;
+use App\Service\User\UserRegisterService;
+
+class UserRegisterOutputDataTransformer implements DataTransformerInterface
+{
+    private UserRegisterService $userRegisterService;
+
+    public function __construct(UserRegisterService $userRegisterService)
+    {
+        $this->userRegisterService = $userRegisterService;
+    }
+
+    /**
+     * @param User $data
+     */
+    public function transform($data, string $to, array $context = [])
+    {
+        $userOutputDto = $this->userRegisterService->createUserOutputDto($data);
+
+        return $userOutputDto;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return UserRegisterOutput::class === $to && $data instanceof User;
+    }
+}
