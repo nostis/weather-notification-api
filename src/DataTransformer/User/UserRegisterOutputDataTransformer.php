7e953cdd1a188b9021d5ae84d17e5e69cb2eb48b
nostis <norbert_2000@o2.pl>
2022-02-16 16:57:08 +0100

    Apply refactored change (static method)

--- src/DataTransformer/User/UserRegisterOutputDataTransformer.php
+++ src/DataTransformer/User/UserRegisterOutputDataTransformer.php
@@ -5,25 +5,16 @@ namespace App\DataTransformer\User;
 use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
 use App\Dto\User\UserAccountOutput;
 use App\Entity\User;
-use App\Service\User\UserRegisterService;
+use App\Service\User\AbstractUserService;
 
 class UserRegisterOutputDataTransformer implements DataTransformerInterface
 {
-    private UserRegisterService $userRegisterService;
-
-    public function __construct(UserRegisterService $userRegisterService)
-    {
-        $this->userRegisterService = $userRegisterService;
-    }
-
     /**
      * @param User $data
      */
     public function transform($data, string $to, array $context = [])
     {
-        $userOutputDto = $this->userRegisterService->createUserOutputDto($data);
-
-        return $userOutputDto;
+        return AbstractUserService::createUserOutputDto($data);
     }
 
     /**
