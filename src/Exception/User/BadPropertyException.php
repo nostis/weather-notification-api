081f0a3d0148286bb125b4c692bf297e7bade48f
nostis <norbert_2000@o2.pl>
2022-02-16 17:59:40 +0100

    Change exception code

--- src/Exception/User/BadPropertyException.php
+++ src/Exception/User/BadPropertyException.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class BadPropertyException extends HttpException
 {
-    public function __construct(int $statusCode = Response::HTTP_BAD_REQUEST, ?string $message = 'Dynamic property not found', \Throwable $previous = null, array $headers = [], ?int $code = 0)
+    public function __construct(int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR, ?string $message = 'Dynamic property not found', \Throwable $previous = null, array $headers = [], ?int $code = 0)
     {
         parent::__construct($statusCode, $message, $previous, $headers, $code);
     }
