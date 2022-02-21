bd58e627c28da956aabca40c41a503b7a7b64746
nostis <norbert_2000@o2.pl>
2022-02-21 17:13:48 +0100

    Create simple model for storing subject and content

---
+++ src/Model/Notification.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model;
+
+class Notification
+{
+    private ?string $subject;
+
+    private ?string $content;
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): void
+    {
+        $this->subject = $subject;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): void
+    {
+        $this->content = $content;
+    }
+}
